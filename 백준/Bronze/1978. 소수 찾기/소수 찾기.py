95a086afe8188bc5911f5a2fe790918e81bc6256
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-04-07 23:38:11 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,22 +1,19 @@
 n = int(input())
-nums = list(map(int, input().split()))
+nums = map(int, input().split())
 
-# 소수란 1과 7로만 자신을 나눌 수 있는 수
-result = 0
-for num in nums:
-    is_prime = True
-
-    # 1은 소수가 아니다
+def check_prime(num):
     if num == 1:
-        is_prime = False
+        return False
 
     for i in range(2, num):
         if num % i == 0:
-            is_prime = False
-            break;
+            return False
+    return True
 
+result = 0
+for num in nums:
+    is_prime = check_prime(num)
     if is_prime:
         result += 1
 
 print(result)
-    
