ad6cf6ef0e42aff4844f31017bb32464562afbfe
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-04-17 10:21:57 +0900

    [Gold V] Title: 두 용액, Time: 132 ms, Memory: 42176 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -6,20 +6,19 @@ a.sort()
 
 pl = 0
 pr = n - 1
-diff = abs(a[pl] + a[pr])
-result = [a[pl], a[pr]]
-
+result = [0, 0]
+diff = float('inf')
 while pl < pr:
     sum = a[pl] + a[pr]
-    if abs(sum) < diff:
+    if diff > abs(sum):
+        result[0], result[1] = a[pl], a[pr]
         diff = abs(sum)
-        result[0] = a[pl]
-        result[1] = a[pr]
-        if sum == 0:
-            break
-    if sum > 0:
+    if sum == 0:
+        break
+    elif sum > 0:
         pr -= 1
     else:
         pl += 1
-
+        
 print(result[0], result[1])
+    
