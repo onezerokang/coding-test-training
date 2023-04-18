89e409d775e0cc6117df965a4adc4a6cc5910b7e
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-04-18 17:20:29 +0900

    [Bronze II] Title: 수 정렬하기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,7 +1,26 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
 
-# 정렬, 중복 x
-a_list = [int(input()) for _ in range(n)]
-a_list.sort()
-for num in a_list:
-    print(num)
+def qsort(a, left, right):
+    pl = left
+    pr = right
+    x = a[(left + right) // 2]
+
+    while pl <= pr:
+        while a[pl] < x: pl += 1
+        while a[pr] > x: pr -= 1
+
+        if pl <= pr:
+            a[pl], a[pr] = a[pr], a[pl]
+            pl += 1
+            pr -= 1
+    
+    if left < pr: qsort(a, left, pr)
+    if pl < right: qsort(a, pl, right)
+
+n = int(input())
+num = [int(input()) for _ in range(n)]
+qsort(num, 0, n - 1)
+
+for x in num:
+    print(x)
