745a79d9a320fc0cde69a578cfac9ca03fa66d46
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-04-27 00:11:01 +0900

    [Gold III] Title: 아침 산책, Time: 1096 ms, Memory: 67856 KB -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-A = list(map(int, input().rstrip()))
+A = [0] + list(map(int, input().rstrip()))
 
 result = 0
 graph = [[] for _ in range(N + 1)]
@@ -11,29 +11,27 @@ for _ in range(N - 1):
     graph[u].append(v)
     graph[v].append(u)
 
-    if A[u - 1] == 1 and A[v - 1] == 1: # 실내가 인접되어 있으면
+    if A[u] == 1 and A[v] == 1: # 실내가 인접되어 있으면
         result += 2 # 경로를 2개 추가한다.
 
 def dfs(start, inside_cnt):
-    visted[start] = True
+    visited[start] = True
 
     for v in graph[start]:
-        if visted[v]:
+        if visited[v]:
             continue
 
-        if A[v - 1] == 1: # 도착 지점이 실내면
+        if A[v] == 1: # 도착 지점이 실내면
             inside_cnt += 1
-            visted[v] = True
         else: # 도착 지점이 실외면
-            inside_cnt += dfs(v, inside_cnt)
+            inside_cnt = dfs(v, inside_cnt)
     
     return inside_cnt
 
-inside_cnt = 0
-visted = [False] * (N + 1)
+visited = [False] * (N + 1)
 for i in range(1, N + 1):
-    if A[i - 1] == 0: # 시작 지점이 실외면
-        inside_cnt = dfs(i, inside_cnt)
+    if A[i] == 0 and not visited[i]: # 시작 지점이 실외면
+        inside_cnt = dfs(i, 0)
+        result += inside_cnt * (inside_cnt - 1)
 
-result += inside_cnt * (inside_cnt - 1)
 print(result)
