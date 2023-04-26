10fa443c55bf484abf0c0629eea235afd34215a8
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-04-26 23:24:56 +0900

    [Gold III] Title: 아침 산책, Time: 1240 ms, Memory: 67752 KB -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -4,16 +4,17 @@ input = sys.stdin.readline
 N = int(input())
 A = list(map(int, input().rstrip()))
 
+result = 0
 graph = [[] for _ in range(N + 1)]
 for _ in range(N - 1):
     u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
 
-result = 0
+    if A[u - 1] == 1 and A[v - 1] == 1: # 실내가 인접되어 있으면
+        result += 2 # 경로를 2개 추가한다.
 
-def dfs(start):
-    global result
+def dfs(start, inside_cnt):
     visted[start] = True
 
     for v in graph[start]:
@@ -21,15 +22,18 @@ def dfs(start):
             continue
 
         if A[v - 1] == 1: # 도착 지점이 실내면
-            result += 1
+            inside_cnt += 1
             visted[v] = True
         else: # 도착 지점이 실외면
-            dfs(v)
+            inside_cnt += dfs(v, inside_cnt)
+    
+    return inside_cnt
 
-
+inside_cnt = 0
+visted = [False] * (N + 1)
 for i in range(1, N + 1):
-    if A[i - 1] == 1: # 시작 지점이 실내면
-        visted = [False] * (N + 1)
-        dfs(i)
+    if A[i - 1] == 0: # 시작 지점이 실외면
+        inside_cnt = dfs(i, inside_cnt)
 
+result += inside_cnt * (inside_cnt - 1)
 print(result)
