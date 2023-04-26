2f10ddf03a0a0a7bdfe5bda9272f65dafb08a4c4
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-04-26 22:28:28 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 52 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -3,26 +3,29 @@ input = sys.stdin.readline
 
 N = int(input())
 nums = list(map(int, input().split()))
-op = list(map(int, input().split()))
+ops = list(map(int, input().split()))
 
-maximum = -1e9
-minimum = 1e9
+def dfs(calc_cnt, total, plus, minus, multiply, divide):
+    global min_val, max_val
 
-def dfs(i, total, plus, minus, multiply, divide):
-    global maximum, minimum
-    if i == N:
-        maximum = max(total, maximum)
-        minimum = min(total, minimum)
-    else:
-        if plus:
-            dfs(i + 1, total + nums[i], plus - 1, minus, multiply, divide)
-        if minus:
-            dfs(i + 1, total - nums[i], plus, minus - 1, multiply, divide)
-        if multiply:
-            dfs(i + 1, total * nums[i], plus, minus, multiply - 1, divide)
-        if divide:
-            dfs(i + 1, int(total / nums[i]), plus, minus, multiply, divide - 1)
+    if calc_cnt == N:
+        if total < min_val:
+            min_val = total
+        if total > max_val:
+            max_val = total
+        return
+    
+    if plus:
+        dfs(calc_cnt + 1, total + nums[calc_cnt], plus - 1, minus, multiply, divide)
+    if minus:
+        dfs(calc_cnt + 1, total - nums[calc_cnt], plus, minus - 1, multiply, divide)
+    if multiply:
+        dfs(calc_cnt + 1, total * nums[calc_cnt], plus, minus, multiply - 1, divide)
+    if divide:
+        dfs(calc_cnt + 1, int(total / nums[calc_cnt]), plus, minus, multiply, divide - 1)
 
-dfs(1, nums[0], op[0], op[1], op[2], op[3])
-print(maximum)
-print(minimum)
+min_val = 1e9
+max_val = -1e9
+dfs(1, nums[0], ops[0], ops[1], ops[2], ops[3])
+print(max_val)
+print(min_val)
