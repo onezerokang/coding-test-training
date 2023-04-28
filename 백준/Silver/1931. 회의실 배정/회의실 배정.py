8a331c932fca8d30491aeb053bbdf61b94e563de
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-04-28 11:21:48 +0900

    [Silver I] Title: 회의실 배정, Time: 244 ms, Memory: 52036 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,16 +1,17 @@
-n = int(input())
-times = [list(map(int, input().split())) for _ in range(n)]
-times.sort(key=lambda x: x[0])
-times.sort(key=lambda x: x[1])
+import sys
+input = sys.stdin.readline
 
-cnt = 0
-last = 0
-for i in range(len(times)):
-    # 앞뒤가 같은 애들은 무조건 호출해야 한다. 조건에 안맞더라도
-    # 운영 시간이 제일 짧은데 손해임
+N = int(input())
+meeting_list = [list(map(int, input().split())) for _ in range(N)]
 
-    if times[i][0] >= last:
-        cnt += 1
-        last = times[i][1]
+meeting_list.sort(key=lambda x: x[0])
+meeting_list.sort(key=lambda x: x[1])
 
-print(cnt)
+end_time = 0
+result = 0
+for x in meeting_list:
+    if x[0] >= end_time:
+        end_time = x[1]
+        result += 1
+
+print(result)
