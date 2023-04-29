4452092931e202395ac54043ec924084fdae7038
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-04-29 11:17:41 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,15 +1,14 @@
-import sys
-input = sys.stdin.readline
+calc_exp = input().split('-')
 
-a = input().rstrip().split('-')
+tmp = []
+for term in calc_exp:
+    tmp_result = 0
+    for num in term.split('+'):
+        tmp_result += int(num)
+    tmp.append(tmp_result)
 
-nums = []
-for i in a:
-    s = map(int, i.split('+'))
-    nums.append(sum(s))
-
-result = nums[0]
-for i in range(1, len(nums)):
-    result -= nums[i]
+result = tmp[0]
+for i in range(1, len(tmp)):
+    result -= tmp[i]
 
 print(result)
