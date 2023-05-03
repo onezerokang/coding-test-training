3cbf1e9ac012d8ef86f13fabe10f5924e34eefce
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-05-03 21:50:02 +0900

    [Gold V] Title: LCS, Time: 544 ms, Memory: 55712 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+A = input().rstrip()
+B = input().rstrip()
+h, w = len(A), len(B)
+dp = [[0] * (w + 1) for _ in range(h + 1)]
+
+for i in range(1, h + 1):
+    for j in range(1, w + 1):
+        if A[i - 1] == B[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
+print(dp[-1][-1])
