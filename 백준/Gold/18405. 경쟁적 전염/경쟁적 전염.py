4112e79171fc9bbdd4185ade166270da9046480f
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-05-07 08:49:15 +0900

    [Gold V] Title: 경쟁적 전염, Time: 120 ms, Memory: 34184 KB -BaekjoonHub

---
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs():
+    queue = deque()
+    for v in virus:
+        queue.append(v)
+    
+    while queue:
+        _type, x, y, time = queue.popleft()
+
+        if time == S:
+            break
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] == 0:
+                queue.append([_type, nx, ny, time + 1])
+                graph[nx][ny] = _type
+
+N, K = map(int, input().split())
+virus = []
+graph = []
+for x in range(N):
+    row = list(map(int, input().split()))
+    graph.append(row)
+
+    for y in range(N):
+        if row[y] > 0:
+            virus.append([row[y], x, y, 0])
+
+virus.sort()
+S, X, Y = map(int, input().split())
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+bfs()
+print(graph[X - 1][Y - 1])
