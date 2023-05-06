bbcaf78575bbc99221e8d166c6b53ffcf53d517c
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-05-07 08:56:16 +0900

    [Gold V] Title: 경쟁적 전염, Time: 108 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -2,25 +2,6 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-def bfs():
-    queue = deque()
-    for v in virus:
-        queue.append(v)
-    
-    while queue:
-        _type, x, y, time = queue.popleft()
-
-        if time == S:
-            break
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] == 0:
-                queue.append((_type, nx, ny, time + 1))
-                graph[nx][ny] = _type
-
 N, K = map(int, input().split())
 
 virus = []
@@ -38,7 +19,27 @@ virus.sort()
 
 S, X, Y = map(int, input().split())
 
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-bfs()
-print(graph[X - 1][Y - 1])
+def bfs(S, X, Y):
+    queue = deque()
+    for v in virus:
+        queue.append(v)
+    
+    while queue:
+        _type, x, y, time = queue.popleft()
+
+        if time == S:
+            break
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] == 0:
+                queue.append((_type, nx, ny, time + 1))
+                graph[nx][ny] = _type
+    return graph[X - 1][Y - 1]
+
+print(bfs(S, X, Y))
