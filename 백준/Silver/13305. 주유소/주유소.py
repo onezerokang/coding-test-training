96b722c1486bbc43951ee8dcea19352916647f85
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-05-12 08:06:58 +0900

    [Silver III] Title: 주유소, Time: 96 ms, Memory: 42684 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -9,10 +9,18 @@ max_price = max(price[:-1])
 min_price = min(price[:-1])
 
 ret = 0
+
 for i in range(N - 1):
-    if price[i] == max_price:
-        ret += price[i] * road[i]
-    elif price[i] == min_price:
+    next_city = 0
+    for j in range(i + 1, N - 1):
+        if price[i] > price[j]:
+            next_city = j
+            break
+    if next_city:
+        ret += price[i] * sum(road[i:next_city])
+        i = next_city
+    else:
         ret += price[i] * sum(road[i:])
+        break
 
 print(ret)
