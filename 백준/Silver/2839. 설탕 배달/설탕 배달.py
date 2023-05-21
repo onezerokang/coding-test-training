0ed2a6da13ff268d1611aed357bb732279e7a5db
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-05-21 19:36:53 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+ans = 0
+
+while N >= 0:
+    if N % 5 == 0:
+        print(ans + N // 5)
+        break
+    N -= 3
+    ans += 1
+else:
+    print(-1)
