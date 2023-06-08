9a4c7bc16fde8c1d2bc53430aff91bcd5dd7c1fe
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-06-08 14:36:56 +0900

    [Silver I] Title: RGB거리, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -5,12 +5,8 @@ N = int(input())
 homes = [list(map(int, input().split())) for _ in range(N)]
 
 for i in range(N - 2, -1, -1):
-    for j in range(3):
-        if j == 0:
-            homes[i][j] += min(homes[i + 1][1], homes[i + 1][2])
-        elif j == 1:
-            homes[i][j] += min(homes[i + 1][0], homes[i + 1][2])
-        else:
-            homes[i][j] += min(homes[i + 1][0], homes[i + 1][1])
+    homes[i][0] += min(homes[i + 1][1], homes[i + 1][2])
+    homes[i][1] += min(homes[i + 1][0], homes[i + 1][2])
+    homes[i][2] += min(homes[i + 1][0], homes[i + 1][1])
 
 print(min(homes[0]))
