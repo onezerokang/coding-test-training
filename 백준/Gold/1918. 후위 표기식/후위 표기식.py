ab70f6b1846f60f9dcdda03c8a18074143909b01
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-06-09 09:52:17 +0900

    [Gold II] Title: 후위 표기식, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.py
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -9,24 +9,16 @@ ans = ""
 for x in a:
     if x.isalpha():
         ans += x
-        continue
-        
-    # 닫는 괄호가 나오면 여는 괄호가 나올 때까지 ans에 더한다.
-    if x == ")":
+    elif x == "(":
+        stack.append(x)
+    elif x == ")":
         while stack[-1] != "(":
             ans += stack.pop()
         stack.pop()
-        continue
-
-    if x == "(":
+    else:
+        while stack and stack[-1] != "(" and operator[stack[-1]] >= operator[x]:
+            ans += stack.pop()
         stack.append(x)
-        continue
-
-    # 마지막 요소보다 우선순위가 낮으면 마지막 요소를 pop하여 ans에 더한다.
-    while stack and stack[-1] != "(" and operator[stack[-1]] >= operator[x]:
-        ans += stack.pop()
-      
-    stack.append(x)
 
 while stack:
     ans += stack.pop()
