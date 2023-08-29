a98b84c859b357246ac41d3d27b6788d99294d24
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2023-08-29 10:17:36 +0900

    [Silver II] Title: 촌수계산, Time: 64 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,32 +1,31 @@
+# DFS 풀이
 from collections import deque
 import sys
 input = sys.stdin.readline
-
-def bfs(graph, visited, start, end):
-    queue = deque([(start, 0)])
-    visited[start] = True
-    
-    while queue:
-        v, d = queue.popleft()
-        
-        for i in graph[v]:
-            if i == end:
-                return d + 1
-                        
-            if not visited[i]:
-                queue.append((i, d + 1))
-                visited[i] = True
-    return -1
-
+           
 n = int(input())
 start, end = map(int, input().split())
 m = int(input())
 graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+result = -1
+
 for _ in range(m):
     x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
 
-visited = [False] * (n + 1)
+def dfs(node, depth):
+    visited[node] = True
 
-print(bfs(graph, visited, start, end))
+    if node == end:
+        global result
+        result = depth
+
+    for i in graph[node]:
+        if not visited[i]:
+            dfs(i, depth + 1)
+
+dfs(start, 0)
+
+print(result)
