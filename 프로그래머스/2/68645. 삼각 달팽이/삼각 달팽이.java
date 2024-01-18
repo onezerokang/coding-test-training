ebe3b748e6196a92b638b6346a913d633ab06f77
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2024-01-18 16:54:31 +0900

    [level 2] Title: 삼각 달팽이, Time: 17.18 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,9 +1,10 @@
+import java.util.*;
+
 class Solution {
     private static final int[] dx = {0, 1, -1};
     private static final int[] dy = {1, 0, -1};
     
-    public int[] solution(int n) {      
-        
+    public int[] solution(int n) {
         int[][] triangle = new int[n][n];
         int v = 1;
         int x = 0;
@@ -19,15 +20,15 @@ class Solution {
                 d = (d + 1) % 3;
                 nx = x + dx[d];
                 ny = y + dy[d];
-                
-                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) break;
+                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+                    break;
+                }
             }
             x = nx;
             y = ny;
         }
         
         int[] result = new int[v - 1];
-        
         int index = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
@@ -38,3 +39,5 @@ class Solution {
         return result;
     }
 }
+
+// 아래 방향
