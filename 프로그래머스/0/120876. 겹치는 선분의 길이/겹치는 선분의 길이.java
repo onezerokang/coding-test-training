9215a6f5bf000235de83600b0b01ab50b92d2a4f
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2024-01-27 20:56:25 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] lines) {        
+        int[] arr1 = compareLines(lines[0], lines[1]);
+        int[] arr2 = compareLines(lines[0], lines[2]);
+        int[] arr3 = compareLines(lines[1], lines[2]);
+        
+        int[] result = new int[201];
+        fillLine(result, arr1);
+        fillLine(result, arr2);
+        fillLine(result, arr3);
+        
+        int answer = 0;
+        for (int num: result) {
+            if (num == 1) answer++;
+        }
+        
+        return answer;
+    }
+    
+    private int[] compareLines(int[] line1, int[] line2) {
+        int[] result = new int[2];
+        if (line1[1] > line2[0]) {
+            result[0] = line1[0] > line2[0] ? line1[0] : line2[0];
+            result[1] = line1[1] < line2[1] ? line1[1] : line2[1];
+        }
+        
+        return result;
+    }
+    
+    private void fillLine(int[] result, int[] line) {
+        for (int i = line[0] + 100; i < line[1] + 100; i++) {
+            result[i] = 1;
+        }
+    }
+}
