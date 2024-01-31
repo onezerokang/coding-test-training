a674dd2d044118f9c4b7198f3fda7b8df2d02d91
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2024-01-31 17:12:05 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 20.60 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,25 +1,16 @@
 class Solution {
-    private int countZero(String s) {
-        int count = 0;
-        for (char c: s.toCharArray()) {
-            if (c == '0') {
-                count++;
-            }
-        }
-        
-        return count;
-    }
-    
     public int[] solution(String s) {
         int[] result = new int[2];
         while (!s.equals("1")) {
-            int zeroCount = countZero(s);
             result[0] += 1;
-            result[1] += zeroCount;
-            s = Integer.toString(s.length() - zeroCount, 2);
+            
+            int before = s.length();
+            s = s.replaceAll("0", "");
+            int after = s.length();
+            result[1] += before - after;
+            
+            s = Integer.toString(s.length(), 2);
         }
-        
-        
         return result;
     }
 }
