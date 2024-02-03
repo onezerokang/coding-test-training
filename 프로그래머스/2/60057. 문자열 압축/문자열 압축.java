e6260daa1870cc91704d4531d7d80ae1473fe5f7
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2024-02-03 17:23:41 +0900

    [level 2] Title: 문자열 압축, Time: 0.08 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,23 +1,25 @@
 import java.util.*;
 
 class Solution {
-    private List<String> split(String source, int length) {
+    private List<String> split(String s, int unit) {
         List<String> tokens = new ArrayList<>();
-        for (int startIndex = 0; startIndex < source.length(); startIndex += length) {
-            int endIndex = startIndex + length;
-            if (endIndex >= source.length()) {
-                endIndex = source.length();
+        for (int startIndex = 0; startIndex < s.length(); startIndex += unit) {
+            int endIndex = startIndex + unit;
+            if (endIndex >= s.length()) {
+                endIndex = s.length();
             }
-            tokens.add(source.substring(startIndex, endIndex));
+            tokens.add(s.substring(startIndex, endIndex));
         }
+        
         return tokens;
     }
     
-    private int compress(String source, int length) {
+    private int compress(String s, int unit) {
         StringBuilder builder = new StringBuilder();
         String last = "";
         int count = 0;
-        for (String token: split(source, length)) {
+        
+        for (String token: split(s, unit)) {
             if (token.equals(last)) {
                 count++;
             } else {
@@ -27,7 +29,6 @@ class Solution {
                 builder.append(last);
                 last = token;
                 count = 1;
-                
             }
         }
         
@@ -35,16 +36,15 @@ class Solution {
             builder.append(count);
         }
         builder.append(last);
-        
         return builder.length();
     }
     
     public int solution(String s) {
-        int minLength = Integer.MAX_VALUE;
+        int result = Integer.MAX_VALUE;
         for (int i = 1; i <= s.length(); i++) {
-            int compressed = compress(s, i);
-            minLength = minLength > compressed ? compressed : minLength;
+            result = Math.min(result, compress(s, i));
         }
-        return minLength;
+        
+        return result;
     }
 }
