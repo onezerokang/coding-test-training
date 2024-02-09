5288cd4c1d4b19e82a3c07700efbf17163c94c91
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2024-02-09 15:36:38 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 14.79 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,52 +1,70 @@
 class Solution {
-    private int[] compress(int[][] arr, int x, int y, int len) {
-        // 종료조건1: 한 변의 길이가 1일 경우
+    private Count compress(Count[] arr) {
+        Count result = new Count(0, 0);
+        for (Count count: arr) {
+            result.zero += count.zero;
+            result.one += count.one;
+        }
+        return result;
+    }
+    
+    private Count compress(int[][] arr, int x, int y, int len) {
         if (len == 1) {
-            return arr[y][x] == 0 ? new int[]{1, 0} : new int[]{0 ,1};
+            if (arr[y][x] == 1) {
+                return new Count(0, 1);
+            } else {
+                return new Count(1, 0);
+            }
         }
         
         int half = len / 2;
-        int[] square1 = compress(arr, x, y, half);
-        int[] square2 = compress(arr, x + half, y, half);
-        int[] square3 = compress(arr, x, y + half, half);
-        int[] square4 = compress(arr, x + half, y + half, half);
+        Count[] compressed = new Count[]{
+            compress(arr, x, y, half),
+            compress(arr, x + half, y, half),
+            compress(arr, x, y + half, half),
+            compress(arr, x + half, y + half, half)    
+        };
         
-        // 종료조건2: 하위 정사각형들에 0이 하나도 없을 경우
-        if (square1[0] == 0 && square2[0] == 0 && square3[0] == 0 && square4[0] == 0) {
-            return new int[]{0, 1};
-        }
-        
-        // 종료조건3: 하위 정사각형들에 1이 하나도 없을 경우
-        if (square1[1] == 0 && square2[1] == 0 && square3[1] == 0 && square4[1] == 0) {
-            return new int[]{1, 0};
-        }
-        
-        int[] result = new int[2];
-        result[0] = square1[0] + square2[0] + square3[0] + square4[0];
-        result[1] = square1[1] + square2[1] + square3[1] + square4[1];
-        
+        Count result = compress(compressed);
+        if (result.zero == 0) return new Count(0, 1);
+        if (result.one == 0) return new Count(1, 0);
         return result;
     }
+    
+    private static class Count {
+        int zero;
+        int one;
         
+        public Count(int zero, int one) {
+            this.zero = zero;
+            this.one = one;
+        }
+        
+        public int[] toArray() {
+            return new int[]{this.zero, this.one};
+        }
+    }
+
+    
     public int[] solution(int[][] arr) {
-        return compress(arr, 0, 0, arr.length);
+        Count count = compress(arr, 0, 0, arr.length);
+        return count.toArray();
     }
 }
 
+// 최종적으로 남는 0의 개수와 1의 개수를 반환해야 한다.
 
-// 각자 0과 1이 몇개 남았는지를 반환해야 함.
+// 상태 정의
+// (x, y, length) -> 해당 정사각형에서 0과 1이 몇개인가
 
-// arr 행의 개수는 1 ~ 1024 이하이며, 2의 거듭 제곱수 형태
+// 종료 조건
+// (x, y, length) = [0, 1] -> 1밖에 없거나
+// (x, y, length) = [1, 0] -> 0밖에 없음
+// (x, y, 1) -> 정사각형 길이가 1임
 
-// 상태 정의: (x좌표, y좌표, 한변길이)
-
-// 종료 조건: 
-// - (x좌표, y좌표, 1) = arr[y좌표][x좌표]
-
-// 점화식
-// (x좌표, y좌표, 한변길이) =
-// (x좌표, y좌표, 한변길이 / 2) +
-// (x좌표 + 한변길이 / 2, y좌표, 한변길이 / 2) +
-// (x좌표, y좌표 + 한변길이 / 2, 한변길이 / 2) +
-// (x좌표 + 한변길이 / 2, y좌표 + 한변길이 / 2, 한변길이 / 2)
-
+// 점화식(부분 문제로 현재 문제를 구성), 상태의 전이
+// (x, y, length) = 
+// (x, y, length / 2) +
+// (x + length / 2, y, length / 2) +
+// (x, y + length / 2, length / 2) +
+// (x + length / 2, y + length / 2, length / 2)
