7c83685ee7391cd779bb42550a3854b1022b883d
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2024-02-11 19:15:20 +0900

    [level 2] Title: H-Index, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,12 +1,26 @@
 class Solution {
+    private boolean isValid(int[] citations, int h) {
+        int count = 0;
+        for (int citation: citations) {
+            if (citation >= h) count++;
+        }
+        return count >= h;
+    }
+    
     public int solution(int[] citations) {
-        for (int h = 1; h <= 1000; h++) {
-            int count = 0;
-            for (int citation: citations) {
-                if (citation >= h) count++;
-            }
-            if (count < h) return h - 1;
+        for (int h = citations.length; h >= 1; h--) {
+            if (isValid(citations, h)) return h;
         }
         return 0;
     }
+    // public int solution(int[] citations) {
+    //     for (int h = 1; h <= 1000; h++) {
+    //         int count = 0;
+    //         for (int citation: citations) {
+    //             if (citation >= h) count++;
+    //         }
+    //         if (count < h) return h - 1;
+    //     }
+    //     return 0;
+    // }
 }
