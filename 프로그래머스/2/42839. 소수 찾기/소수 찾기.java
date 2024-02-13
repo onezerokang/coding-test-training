a65b1d9871f2abbc2b2da372be5159c08271f328
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2024-02-13 20:25:44 +0900

    [level 2] Title: 소수 찾기, Time: 3.67 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,36 +1,47 @@
-import java.util.stream.*;
 import java.util.*;
 
 class Solution {
-    private boolean isPrime(int n) {
-        if (n <= 1) return false;
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) return false;
+    private boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
         }
         return true;
     }
     
-    private Set<Integer> getPrimes(int acc, List<Integer> numbers) {
-        Set<Integer> set = new HashSet<>();
-        if (isPrime(acc)) {
-            set.add(acc);
-        }
-    
-        for (int i = 0; i < numbers.size(); i++) {
-            int nextAcc = acc * 10 + numbers.get(i);
-            List<Integer> copiedNumbers = new ArrayList<>(numbers);
-            copiedNumbers.remove(i);
-            set.addAll(getPrimes(nextAcc, copiedNumbers));
+    private void getPrimes(
+        Set<Integer> acc, String numbers, List<Character> others) {
+        if (isPrime(Integer.parseInt(numbers))) acc.add(Integer.parseInt(numbers));
+        if (others.isEmpty()) return;
+        
+        for (Character next: others) {
+            List<Character> nextOthers = new ArrayList<>(others);
+            nextOthers.remove(next);
+            getPrimes(acc, numbers + next, nextOthers);
         }
         
-        return set;
     }
     
     public int solution(String numbers) {
-        List<Integer> nums = numbers.chars()
-            .map(c -> c - '0')
-            .boxed()
-            .collect(Collectors.toList());
-        return getPrimes(0, nums).size();
+        Set<Integer> set = new HashSet<>();
+        List<Character> others = new LinkedList<>();
+        for (Character c: numbers.toCharArray()) {
+            others.add(c);
+        }
+        
+        getPrimes(set, "0", others);
+        return set.size();
     }
 }
+
+// 상태
+// (이어붙인숫자, 선택가능숫자)
+
+// 종료조건
+// (이어붙인숫자, empty) -> List<Integer>
+
+// 점화식
+// (이어붙인숫자, 선택가능숫자) = (이어붙인숫자, 선택가능숫자) + (이어붙인숫자, 선택가능숫자)
+
+// 종이조각을 붙여 소수 몇개 만들 수 있음?
+// 완전탐색으로 모든 소수 다 구하고 -> 소수인지 검사
