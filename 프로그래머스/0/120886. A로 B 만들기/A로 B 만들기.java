75e170151f9f2cb88ddcba0f898df0308a6c9bc1
Wonyoung Kang <60874549+onezerokang@users.noreply.github.com>
2024-02-19 16:04:04 +0900

    [level 0] Title: A로 B 만들기, Time: 0.52 ms, Memory: 69.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String before, String after) {
+        char[] b = before.toCharArray();
+        char[] a = after.toCharArray();
+        Arrays.sort(b);
+        Arrays.sort(a);
+        
+        for (int i = 0; i < b.length; i++) {
+            if (b[i] != a[i]) return 0;
+        }
+        
+        return 1;
+    }
+}
