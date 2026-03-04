14757430db311a2ee70ed45e6483e46fffadae4c
onezerokang <60874549+onezerokang@users.noreply.github.com>
2026-03-04 22:19:43 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 5.44 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,12 +1,7 @@
 import java.util.*;
 
 class Solution {    
-    // 숫자와 연산자를 분리해서 리스트에 담는다.
-    // 연산자 우선순위 조합을 구한다.
-    // 연산자 우선순위를 순회하면서 합을 구한다.
-    // 가장 높은합을 반환한다.
     private static final List<String> operators = List.of("+", "-", "*");
-    
     private static final List<List<String>> operatorCombination = List.of(
         List.of("*", "+", "-"),
         List.of("*", "-", "+"),
@@ -21,13 +16,7 @@ class Solution {
         for (List<String> operators : operatorCombination) {
             Calculator calculator = new Calculator(expression);
             for (String operator : operators) {
-                if (operator.equals("+")) {
-                    calculator.plus();
-                } else if (operator.equals("-")) {
-                    calculator.minus();
-                } else if (operator.equals("*")) {
-                    calculator.multiply();
-                }
+                calculator.calculate(operator);
             }   
             answer = Math.max(answer, calculator.getResult());
         }
@@ -53,12 +42,15 @@ class Solution {
             );
         }
         
-        private void plus() {
+        private void calculate(String operator) {
             int index = 0;
             while (true) {
-                if (expression.get(index).equals("+")) {
-                    long tmp = Long.valueOf(result.get(result.size() - 1))
-                        + Long.valueOf(expression.get(index + 1));
+                if (operator.equals(expression.get(index))) {
+                    long tmp = calculate(
+                        expression.get(index),
+                        Long.valueOf(result.get(result.size() - 1)),
+                        Long.valueOf(expression.get(index + 1))
+                    );
                     result.remove(result.size() - 1);
                     result.add(String.valueOf(tmp));
                     index++;
@@ -74,47 +66,13 @@ class Solution {
             this.result.clear();
         }
         
-        private void minus() {
-            int index = 0;
-            while (true) {
-                if (expression.get(index).equals("-")) {
-                    long tmp = Long.valueOf(result.get(result.size() - 1))
-                        - Long.valueOf(expression.get(index + 1));
-                    System.out.println(tmp);
-                    result.remove(result.size() - 1);
-                    result.add(String.valueOf(tmp));
-                    index++;
-                } else {
-                    result.add(expression.get(index));
-                }
-                if (++index >= expression.size()) {
-                    break;
-                }
-            }
-            this.expression.clear();
-            this.expression.addAll(this.result);
-            this.result.clear();
-        }
-        
-        private void multiply() {
-            int index = 0;
-            while (true) {
-                if (expression.get(index).equals("*")) {
-                    long tmp = Long.valueOf(result.get(result.size() - 1))
-                        * Long.valueOf(expression.get(index + 1));
-                    result.remove(result.size() - 1);
-                    result.add(String.valueOf(tmp));
-                    index++;
-                } else {
-                    result.add(expression.get(index));
-                }
-                if (++index >= expression.size()) {
-                    break;
-                }
-            }
-            this.expression.clear();
-            this.expression.addAll(this.result);
-            this.result.clear();
+        private long calculate(String operator, long num1, long num2) {
+            return switch(operator) {
+                case "+" -> num1 + num2;
+                case "-" -> num1 - num2;
+                case "*" -> num1 * num2;
+                default -> throw new RuntimeException("NotAllowedOperator");
+            };
         }
         
         public long getResult() {
